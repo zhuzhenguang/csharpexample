06068bd17c93a6a39f63dd8f1861e56c4fb6f1b5
zhuzhenguang <zhuzhenguang2008@gmail.com>
2019-11-28 07:58:53 +0800

    init project

---
+++ csharpexample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace csharpexample
+{
+    class Program
+    {
+        private static readonly object lockObject = new object();
+        private static int account;
+
+        static async Task Main(string[] args)
+        {
+            Task task1 = Task.Run(Add10K);
+            Task task2 = Task.Run(Add10K);
+
+            await task1;
+            await task2;
+
+            Console.WriteLine(account);
+        }
+
+        static void Add10K()
+        {
+            lock (lockObject)
+            {
+                Enumerable.Range(0, 10000).ToList().ForEach(i => account += 1);
+            }
+        }
+    }
+}
