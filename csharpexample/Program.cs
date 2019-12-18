b2fc28047a7b102633a86b567b5505b287683697
zhuzhenguang <zhuzhenguang2008@gmail.com>
2019-12-19 07:14:26 +0800

    compare and change

--- csharpexample/Program.cs
+++ csharpexample/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace csharpexample
@@ -8,23 +11,50 @@ namespace csharpexample
     {
         private static readonly object lockObject = new object();
         private static int account;
+        private static int usingResource = 0;
 
         static async Task Main(string[] args)
         {
-            Task task1 = Task.Run(Add10K);
-            Task task2 = Task.Run(Add10K);
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            // Task task1 = Task.Run(Add10KWithCompareExchnage);
+            // Task task2 = Task.Run(Add10KWithCompareExchnage);
+            // Task task1 = Task.Run(Add10KWithLock);
+            // Task task2 = Task.Run(Add10KWithLock);
+            List<Task> tasks = Enumerable.Range(0, 20000).ToList().Select(i => Task.Run(Add1WithCompareExchnage)).ToList();
+            
+            foreach (Task task in tasks)
+            {
+                await task;
+            }
 
-            await task1;
-            await task2;
+            // await task1;
+            // await task2;
 
             Console.WriteLine(account);
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.ElapsedMilliseconds);
         }
 
-        static void Add10K()
+        static void Add10KWithLock()
         {
             lock (lockObject)
             {
                 Enumerable.Range(0, 10000).ToList().ForEach(i => account += 1);
+            }
+        }
+
+        static void Add1WithCompareExchnage()
+        {
+            while (true)
+            {
+                int currentAccount = account;
+                int newAccount = account + 1;
+                int replaced = Interlocked.CompareExchange(ref account, newAccount, currentAccount);
+                if (replaced == currentAccount)
+                {
+                    break;
+                }
             }
         }
     }
